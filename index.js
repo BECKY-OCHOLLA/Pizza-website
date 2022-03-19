3d8672be2ca8c5b3a02838b7b8b4c7ad2dd368e8
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-03-19 13:54:45 +0300

    it will add variables to te bl

---
+++ index.js
@@ -0,0 +1,16 @@
+var pizzaSize={
+    large:1000,
+    medium:800,
+    small:550
+};
+var crust={
+    cripsy:100,
+    stuffed:120,
+    gluten:50
+
+};
+var toppings={
+    veggie:100,
+    traditional:200,
+    speciality: 250
+};
