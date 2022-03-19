d192ec187121598d2b25a1218e44eef9cd88c3da
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-03-19 15:39:47 +0300

    it will add more functions

--- index.js
+++ index.js
@@ -3,14 +3,54 @@ var pizzaSize={
     medium:800,
     small:550
 };
-var crust={
+var pizzaCrust={
     cripsy:100,
     stuffed:120,
     gluten:50
 
 };
-var toppings={
+var pizzaToppings={
     veggie:100,
     traditional:200,
     speciality: 250
 };
+function sizePizzaPrice(size){
+    if(size ==="large"){
+        return pizzaSize.large*1;
+    }else if(size==="medium"){
+        return pizzaSize.medium*1
+    }else{
+        return pizzaSize.small*1
+    }
+
+}
+function crustCalcPrice(crust) {
+    if (crust === "crispy") {
+        return pizzaCrust.crispy * 1;
+    } else if (crust === "stuffed") {
+        return pizzaCrust.stuffed * 1;
+    } else {
+        return pizzaCrust.gluten * 1;
+    }
+}
+
+
+
+
+
+
+$("document").ready(function() {
+
+    function getPizzaSize() {
+        return $("#pizza-size")
+            .find(":selected")
+            .val();
+    }
+
+    function getCrust() {
+        return $("#pizza-crust")
+            .find(":selected")
+            .val();
+    }
+});
+
